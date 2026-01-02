73c09d15434b28097569fd8dda8dc288ba7f8a53
waileong <wai_leong1015@hotmail.com>
2026-01-02 17:48:13 +0800

    Initial Commit

---
+++ src/main/java/org/example/dependencycheckspringboot4mongodb/DependencyCheckSpringBoot4MongodbApplication.java
@@ -0,0 +1,13 @@
+package org.example.dependencycheckspringboot4mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyCheckSpringBoot4MongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyCheckSpringBoot4MongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dependencycheckspringboot4mongodb/DependencyCheckSpringBoot4MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dependencycheckspringboot4mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyCheckSpringBoot4MongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
